01de1fdc5af66610aef76bae1d521e2ffd1a5d72
Daranivelan <daranivelan10@gmail.com>
2025-08-08 12:54:27 +0530

    added vercel configs

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import cartRoutes from "./routes/cart.route";
 import favRoutes from "./routes/fav.route";
 import cors from "cors";
 import { User as IUser } from "./types/auth";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 declare global {
   namespace Express {
@@ -30,7 +31,11 @@ app.use("/api/games", gameRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/favourites", favRoutes);
 
-app.listen(3000, () => {
-  connectDB();
-  console.log("App is Running...!");
-});
+// app.listen(3000, () => {
+//   connectDB();
+//   console.log("App is Running...!");
+// });
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  app(req as any, res as any); // Express needs Node's req/res
+};
